aabfa4780beb6f2c114bda749268ee8b37e797f3
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2026-03-29 17:40:56 -0700

    Initial Commit

---
+++ gurbani/gurbaniApp.swift
@@ -0,0 +1,32 @@
+//
+//  gurbaniApp.swift
+//  gurbani
+//
+//  Created by Sukhman Singh on 3/29/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct gurbaniApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
